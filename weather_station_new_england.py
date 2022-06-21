b31492d8a4e7db9cf601cec53c077e6899be08b7
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-06-21 13:07:48 -0400

    finish section 1 - 3

--- weather_station_new_england.py
+++ weather_station_new_england.py
@@ -1,12 +1,13 @@
 from turtle import st, ycor
 from matplotlib.style import available
+from tqdm import tqdm
 import requests
 import json
 
 available_cor = [(a * 0.01, b * 0.01) for a in range(4098, 4733, 2) for b in range(-7372, -6695, 2)]
 available_station = {}
 
-for each_cor in available_cor[:20]:
+for each_cor in tqdm(available_cor):
     response = requests.request("GET", f"https://api.weather.gov/points/{each_cor[0]},{each_cor[1]}")
     result = response.json()
     if response.status_code == 200:
@@ -20,7 +21,6 @@ for each_cor in available_cor[:20]:
             if (x_cor, y_cor) not in available_station[station_name]:
                 available_station[station_name].append((x_cor, y_cor))
 
-print(available_station)
 
-# with open('test_result.json', 'w') as data_file:
-#     json.dump(out_put, data_file)
+with open('weather_station_new_england.json', 'w') as data_file:
+    json.dump(available_station, data_file)
