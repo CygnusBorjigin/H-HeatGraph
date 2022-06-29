099eb2f6cc53914f58f2e48ed9c56499f008503d
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-06-29 19:42:42 -0400

    data cleaning
    finish location detailed information logic

--- weather_api/base/views.py
+++ weather_api/base/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(requests):
-    return HttpResponse("this is base / home")
+    return render(requests, 'home.html')
     
