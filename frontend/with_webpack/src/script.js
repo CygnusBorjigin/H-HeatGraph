a98a35208e0a60bd7d28fdd6f140837c62b711ba
SomeoneSomewhere <cygnusborjigin@gmail.com>
2022-07-02 16:34:05 -0400

    finish initial visualization logic

--- frontend/with_webpack/src/script.js
+++ frontend/with_webpack/src/script.js
@@ -1,6 +1,38 @@
 import './style.css';
+import { mean, min } from 'mathjs'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import weahterInfo from './data/new_england_temperature.json';
+
+// extract revalent weather information
+let weatherInfoByState = [];
+let coordinateX = [];
+let coordinateY = [];
+let allTemperatures = [];
+
+for (const [stateKey, stateVal] of Object.entries(weahterInfo)) {
+    for (const [gridPointKey, gridPointVal] of Object.entries(stateVal)) {
+        const coordinateString = gridPointKey.split('-');
+        const coordinateTuple = [parseFloat(coordinateString[1]), parseFloat(coordinateString[2])];
+        coordinateX.push(coordinateTuple[0]);
+        coordinateY.push(coordinateTuple[1]);
+        allTemperatures.push(gridPointVal[0].value);
+    }
+}
+
+const xMean = mean(coordinateX);
+const yMean = mean(coordinateY);
+const tempMean = min(allTemperatures);
+
+for (const [stateKey, stateVal] of Object.entries(weahterInfo)) {
+    for (const [gridPointKey, gridPointVal] of Object.entries(stateVal)) {
+        const coordinateString = gridPointKey.split('-');
+        const coordinateTuple = [parseFloat(coordinateString[1]), parseFloat(coordinateString[2])];
+        const temperature = gridPointVal[0].value;
+        console.log(temperature - tempMean);
+        weatherInfoByState.push([coordinateTuple[0] - xMean, coordinateTuple[1] - yMean, temperature - (tempMean/2)]);
+    }
+}
 
 // obtain the canvas
 const canvas = document.getElementById("canvas");
@@ -9,9 +41,15 @@ const canvas = document.getElementById("canvas");
 const scene = new THREE.Scene();
 
 // create the object
+weatherInfoByState.forEach(each_point => {
+    const [xValue, yValue, zValue] = each_point;
+    const tempBox = new THREE.Mesh(new THREE.BoxGeometry(0.1, 0.1, zValue), new THREE.MeshBasicMaterial({color: "blue"}))
+    scene.add(tempBox);
+    tempBox.position.set(yValue * 10, xValue * 10, zValue);
+})
 
-const box1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: "blue"}));
-scene.add(box1);
+// const box1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: "blue"}));
+// scene.add(box1);
 
 // create the camera
 const sizes = {
@@ -51,8 +89,6 @@ const clock = new THREE.Clock();
 const tick = () => {
     const deltaTime = clock.getElapsedTime();
 
-    //box1.rotation.x = deltaTime;
-    //box1.rotation.y = deltaTime;
     renderer.render(scene, camera);
     window.requestAnimationFrame(tick)
 };
